a64c195534f273f571723c5398cae2150d0d6d2b
Prasanna Loganathar <pvl@prasannavl.com>
2019-05-26 20:01:17 +0530

    add logind on lid ext

---
+++ sysconfig/logind-config-lid-on-external-power.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+file=/etc/systemd/logind.conf
+
+# options: suspend (default), ignore, etc
+val=${1:-ignore}
+
+m='^#?HandleLidSwitchExternalPower=.*$'
+r="HandleLidSwitchExternalPower=${val}\n"
+pattern="s/${m}/${r}/"
+
+echo "> original from: $file"
+grep -P "${m}" $file
+
+echo "processing: $file"
+sudo sed -i -E "${pattern}" $file
+
+printf "\n> processed:\n"
+grep -P "${m}" $file
