77f046ea4bdd9b3ca386261ff08fcd2a76e9d11a
Prasanna Loganathar <pvl@prasannavl.com>
2019-07-10 12:56:01 +0530

    update sysconfig/ubuntu/gnome-reset-workspace-keybindings.sh

--- sysconfig/ubuntu/gnome-reset-workspace-keybindings.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-up "['<Super>Page_Up']"
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-down "['<Super>Page_Down']"
