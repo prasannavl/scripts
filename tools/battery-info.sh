96dffe40c3eec90d38a9ab977240a303b429990b
Prasanna Loganathar <pvl@prasannavl.com>
2020-01-13 21:10:55 +0530

    add battery info tool

---
+++ tools/battery-info.sh
@@ -0,0 +1,10 @@
+# show battery info
+
+upower -i /org/freedesktop/UPower/devices/battery_BAT0
+
+# alternate options
+  
+# https://askubuntu.com/questions/69556/how-do-i-check-the-batterys-status-via-the-terminal/102863
+# 
+# grep POWER_SUPPLY_CAPACITY /sys/class/power_supply/BAT1/uevent
+# acpi -V 
