37bd03c7248e2f7c4c282385d35ae873dd7379e4
Prasanna Loganathar <pvl@prasannavl.com>
2020-06-15 13:37:21 +0530

    Add nvidia doc links

--- tools/nvx.sh
+++ tools/nvx.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # Nvidia optimus
+# https://download.nvidia.com/XFree86/Linux-x86_64/440.82/README/primerenderoffload.html
+# https://download.nvidia.com/XFree86/Linux-x86_64/440.82/README/dynamicpowermanagement.html
 
-# __GL_SYNC_TO_VBLANK=0 for vsync off
+# __GL_SYNC_TO_VBLANK=0 for vsync off 
 
 __NV_PRIME_RENDER_OFFLOAD=1 __GLX_VENDOR_LIBRARY_NAME=nvidia "$@"
